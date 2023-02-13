fe2caf68dc9a83ea146c57d9691225e78a425c7c
Dony <dony.kg1@gmail.com>
2023-02-13 22:06:31 +0600

    Add programm

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше или равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+
+using static System.Console;
+
+WriteLine("Введите массив через пробел: ");
+string[] array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+WriteLine();
+Console.WriteLine("[{0}]", string.Join(", ", SelectArray(array)));
+
+
+string[] SelectArray(string[] inArray) // формирование нового массива
+{
+    string[] resultArray = new string[NewArraySize(inArray)];
+    int count = 0;
+    foreach (var item in inArray)
+    {
+        if (item.Length <= 3)
+        {
+            resultArray[count] = item;
+            count++;
+        }
+    }
+    return resultArray;
+
+}
+
+
+int NewArraySize(string[] inArray) //определения длинны нового массива
+{
+    int result = 0;
+    foreach (var item in inArray)
+    {
+        if (item.Length <= 3)
+            result++;
+    }
+    return result;
+}
